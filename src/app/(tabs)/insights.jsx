4c1ca39e412f88a22fb291f5295ab134bfeb3411
Diwakar pandey <diwakarpandey410@gmail.com>
2026-07-09 12:19:02 +0530

    feat: add insights screen components and clear completed button functionality

--- src/app/(tabs)/insights.jsx
+++ src/app/(tabs)/insights.jsx
@@ -1,11 +1,33 @@
-import { View } from "react-native";
-import { UserButton } from '@clerk/expo/native'
-const insights = () => {
+import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
+import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
+import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
+import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
+// import SentryFeedbackButton from "@/components/insights/SentryFeedbackButton";
+import UserProfile from "@/components/insights/UserProfile";
+import TabScreenBackground from "@/components/TabScreenBackground";
+import { ScrollView } from "react-native";
+
+const InsightsScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-     <UserButton />
-    </View>
+    <>
+      <ScrollView
+        className="flex-1 bg-background py-4"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20, gap: 14 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TabScreenBackground />
+
+        <UserProfile />
+        <InsightsStatsSection />
+        <InsightsCategorySection />
+        <InsightsPrioritySection />
+        <ClearCompletedButton />
+      </ScrollView>
+
+      {/* <SentryFeedbackButton /> */}
+    </>
   );
 };
 
-export default insights;
+export default InsightsScreen;
